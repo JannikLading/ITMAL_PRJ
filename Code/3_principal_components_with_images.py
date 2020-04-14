3a07ea6c9a3599fc046949ece711d8b206793f0b
Alexander Bjerre <alexander8@hotmail.com>
2020-04-14 15:32:49 +0200

    3 principal components reworked to work in MAL project. Also compares original pristine dataset with new cleaned up dataset, where some outliers have been removed.

--- Code/3_principal_components_with_images.py
+++ Code/3_principal_components_with_images.py
@@ -21,10 +21,14 @@ from sklearn.preprocessing import scale
 # Paths
 #path_working_dir = "C:/Users/flole/Desktop/dev/MAL/itmal/Own_Prj/rps-cv-images/"
 path_working_dir = os.path.dirname(os.path.realpath(__file__))
-path_paper = path_working_dir + "\\paper\\"
-path_rock = path_working_dir + "\\rock\\"
-path_scissors = path_working_dir + "\\scissors\\"
+path_paper = path_working_dir + "\\..\\rockpaperscissors\\paper\\"
+path_rock = path_working_dir + "\\..\\rockpaperscissors\\rock\\"
+path_scissors = path_working_dir + "\\..\\rockpaperscissors\\scissors\\"
 
+# Original dataset for comparison
+path_paper_pristine = path_working_dir + "\\..\\rockpaperscissors\\rps-cv-images\\paper\\"
+path_rock_pristine = path_working_dir + "\\..\\rockpaperscissors\\rps-cv-images\\rock\\"
+path_scissors_pristine = path_working_dir + "\\..\\rockpaperscissors\\rps-cv-images\\scissors\\"
 
 def loadImages(path):
     # return array of images
@@ -41,7 +45,12 @@ rock_imgs = loadImages(path_rock)
 paper_imgs = loadImages(path_paper)
 scissors_imgs = loadImages(path_scissors)
 
+rock_imgs_pristine = loadImages(path_rock_pristine)
+paper_imgs_pristine = loadImages(path_paper_pristine)
+scissors_imgs_pristine = loadImages(path_scissors_pristine)
+
 imgs = rock_imgs + paper_imgs + scissors_imgs
+imgs_pristine = rock_imgs_pristine + paper_imgs_pristine + scissors_imgs_pristine
 
 # reshape images to not include the rgb parameter (3)
 def reshapeImgs(images):
@@ -55,10 +64,12 @@ def reshapeImgs(images):
     
     return newImages
 
+# Are these lines actually used?
 reshaped_imgs = reshapeImgs(imgs)
+reshaped_imgs_pristine = reshapeImgs(imgs_pristine)
 
 # Testing and showing images
-plt.imshow(imgs[1438])
+# plt.imshow(imgs[1438])
 
 #%%
 # Seeing if we can get some scatter plots with patterns
@@ -71,26 +82,13 @@ rock_imgs_pca3 = pca_3.fit_transform(scale(reshapeImgs(rock_imgs)))
 paper_imgs_pca3 = pca_3.fit_transform(scale(reshapeImgs(paper_imgs)))
 scissors_imgs_pca3 = pca_3.fit_transform(scale(reshapeImgs(scissors_imgs)))
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.scatter(rock_imgs_pca3[:,0], rock_imgs_pca3[:,1], rock_imgs_pca3[:,2], marker='o', color='blue')
-ax.scatter(paper_imgs_pca3[:,0], paper_imgs_pca3[:,1], paper_imgs_pca3[:,2], marker='^', color='green')
-ax.scatter(scissors_imgs_pca3[:,0], scissors_imgs_pca3[:,1], scissors_imgs_pca3[:,2], marker='x', color='orange')
-ax.view_init(180, 130)
+rock_imgs_pca3_pristine = pca_3.fit_transform(scale(reshapeImgs(rock_imgs_pristine)))
+paper_imgs_pca3_pristine = pca_3.fit_transform(scale(reshapeImgs(paper_imgs_pristine)))
+scissors_imgs_pca3_pristine = pca_3.fit_transform(scale(reshapeImgs(scissors_imgs_pristine)))
 
-fig = plt.figure()
-bx = fig.add_subplot(111, projection='3d')
-bx.set_xlabel('X')
-bx.set_ylabel('Y')
-bx.set_zlabel('Z')
-bx.scatter(rock_imgs_pca3[:,0], rock_imgs_pca3[:,1], rock_imgs_pca3[:,2], marker='o', color='blue')
-bx.scatter(paper_imgs_pca3[:,0], paper_imgs_pca3[:,1], paper_imgs_pca3[:,2], marker='^', color='green')
-bx.scatter(scissors_imgs_pca3[:,0], scissors_imgs_pca3[:,1], scissors_imgs_pca3[:,2], marker='x', color='orange')
-bx.view_init(0, 130)
+#%%
 
+# cleaned up 3d plot
 fig = plt.figure()
 cx = fig.add_subplot(111, projection='3d')
 cx.set_xlabel('X')
@@ -99,39 +97,58 @@ cx.set_zlabel('Z')
 cx.scatter(rock_imgs_pca3[:,0], rock_imgs_pca3[:,1], rock_imgs_pca3[:,2], marker='o', color='blue')
 cx.scatter(paper_imgs_pca3[:,0], paper_imgs_pca3[:,1], paper_imgs_pca3[:,2], marker='^', color='green')
 cx.scatter(scissors_imgs_pca3[:,0], scissors_imgs_pca3[:,1], scissors_imgs_pca3[:,2], marker='x', color='orange')
-cx.view_init(-110, 90)
+cx.view_init(0, 0)
+# Pristine 3d plot
+fig = plt.figure()
+cx = fig.add_subplot(111, projection='3d')
+cx.set_xlabel('X')
+cx.set_ylabel('Y')
+cx.set_zlabel('Z')
+cx.scatter(rock_imgs_pca3_pristine[:,0], rock_imgs_pca3_pristine[:,1], rock_imgs_pca3_pristine[:,2], marker='o', color='blue')
+cx.scatter(paper_imgs_pca3_pristine[:,0], paper_imgs_pca3_pristine[:,1], paper_imgs_pca3_pristine[:,2], marker='^', color='green')
+cx.scatter(scissors_imgs_pca3_pristine[:,0], scissors_imgs_pca3_pristine[:,1], scissors_imgs_pca3_pristine[:,2], marker='x', color='orange')
+cx.view_init(0, 0)
 
 
-#%%
-fig, axs = plt.subplots(2, 2)
-axs[0, 0].scatter(rock_imgs_pca3[:,0], rock_imgs_pca3[:,1], rock_imgs_pca3[:,2], projection='3d')
-axs[0, 0].set_title('Rock Pattern')
-axs[0, 1].scatter(paper_imgs_pca3[:,0], paper_imgs_pca3[:,1], paper_imgs_pca3[:,2])
-axs[0, 1].set_title('Paper Pattern')
-axs[1, 0].scatter(scissors_imgs_pca3[:,0], scissors_imgs_pca3[:,1],scissors_imgs_pca3[:,2])
-axs[1, 0].set_title('Scissor Pattern')
+# cleaned up 3d plot
+fig = plt.figure()
+cx = fig.add_subplot(111, projection='3d')
+cx.set_xlabel('X')
+cx.set_ylabel('Y')
+cx.set_zlabel('Z')
+cx.scatter(rock_imgs_pca3[:,0], rock_imgs_pca3[:,1], rock_imgs_pca3[:,2], marker='o', color='blue')
+cx.scatter(paper_imgs_pca3[:,0], paper_imgs_pca3[:,1], paper_imgs_pca3[:,2], marker='^', color='green')
+cx.scatter(scissors_imgs_pca3[:,0], scissors_imgs_pca3[:,1], scissors_imgs_pca3[:,2], marker='x', color='orange')
+cx.view_init(0, 180)
+# Pristine 3d plot
+fig = plt.figure()
+cx = fig.add_subplot(111, projection='3d')
+cx.set_xlabel('X')
+cx.set_ylabel('Y')
+cx.set_zlabel('Z')
+cx.scatter(rock_imgs_pca3_pristine[:,0], rock_imgs_pca3_pristine[:,1], rock_imgs_pca3_pristine[:,2], marker='o', color='blue')
+cx.scatter(paper_imgs_pca3_pristine[:,0], paper_imgs_pca3_pristine[:,1], paper_imgs_pca3_pristine[:,2], marker='^', color='green')
+cx.scatter(scissors_imgs_pca3_pristine[:,0], scissors_imgs_pca3_pristine[:,1], scissors_imgs_pca3_pristine[:,2], marker='x', color='orange')
+cx.view_init(0, 180)
 
-#%%
 
-X_digits_pca2_1 = []
-X_digits_pca2_3 = []
-X_digits_pca2_5 = []
-X_digits_pca2_9 = []
-
-index = 0
-for digit in y_digits[:]:
-    if digit == 1:
-        X_digits_pca2_1.append(X_digits_pca2[index])
-    elif digit == 3:
-        X_digits_pca2_3.append(X_digits_pca2[index])
-    elif digit == 5:
-        X_digits_pca2_5.append(X_digits_pca2[index])
-    elif digit == 9:
-        X_digits_pca2_9.append(X_digits_pca2[index])
-        
-    index = index + 1
-  
-X_digits_pca2_1 = np.array(X_digits_pca2_1[:])
-X_digits_pca2_3 = np.array(X_digits_pca2_3[:])
-X_digits_pca2_5 = np.array(X_digits_pca2_5[:])
-X_digits_pca2_9 = np.array(X_digits_pca2_9[:])
+# cleaned up 3d plot
+fig = plt.figure()
+cx = fig.add_subplot(111, projection='3d')
+cx.set_xlabel('X')
+cx.set_ylabel('Y')
+cx.set_zlabel('Z')
+cx.scatter(rock_imgs_pca3[:,0], rock_imgs_pca3[:,1], rock_imgs_pca3[:,2], marker='o', color='blue')
+cx.scatter(paper_imgs_pca3[:,0], paper_imgs_pca3[:,1], paper_imgs_pca3[:,2], marker='^', color='green')
+cx.scatter(scissors_imgs_pca3[:,0], scissors_imgs_pca3[:,1], scissors_imgs_pca3[:,2], marker='x', color='orange')
+cx.view_init(0, 60)
+# Pristine 3d plot
+fig = plt.figure()
+cx = fig.add_subplot(111, projection='3d')
+cx.set_xlabel('X')
+cx.set_ylabel('Y')
+cx.set_zlabel('Z')
+cx.scatter(rock_imgs_pca3_pristine[:,0], rock_imgs_pca3_pristine[:,1], rock_imgs_pca3_pristine[:,2], marker='o', color='blue')
+cx.scatter(paper_imgs_pca3_pristine[:,0], paper_imgs_pca3_pristine[:,1], paper_imgs_pca3_pristine[:,2], marker='^', color='green')
+cx.scatter(scissors_imgs_pca3_pristine[:,0], scissors_imgs_pca3_pristine[:,1], scissors_imgs_pca3_pristine[:,2], marker='x', color='orange')
+cx.view_init(0, 60)
