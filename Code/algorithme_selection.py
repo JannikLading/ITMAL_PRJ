6d968d67f098a6956b3c9e050a0a20bd64efbcc7
Alexander Bjerre <alexander8@hotmail.com>
2020-04-29 17:11:52 +0200

    code for gridsearch and random search for LinearSVC

--- Code/algorithme_selection.py
+++ Code/algorithme_selection.py
@@ -35,71 +35,60 @@ X_train, X_test = X[:test_train_split_80], X[test_train_split_80:]
 y_train, y_test = y[:test_train_split_80], y[test_train_split_80:]
 
 #%%
+# Running The LinearSVC model
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, train_test_split
+from sklearn.svm import LinearSVC
+from time import time
+#clf = LinearSVC(random_state=0, max_iter=10000)
+#clf.fit(X_imgs_training, y_training)
+#y_pred = clf.predict(X_imgs_test)
 
-## Switch with the classifiers suggested in cheat sheat
+linearsvc_model = LinearSVC()
+linearsvc_tuning_parameters = {
+        "penalty": ('l1','l2'),#"penalty": ("l1", "l2"),
+        "loss": ("hinge", "squared_hinge"),#"loss": ("hinge"),#
+        "dual": [False, True],
+        "tol": [1e-3, 1e-2, 1e-4],
+        "C": [0.2, 0.5, 1, 1.5, 2],
+        "multi_class": ("ovr", "crammer_singer"),
+        "fit_intercept": [False, True],
+        "intercept_scaling": [0.2, 0.5, 1, 1.5, 2],
+        "max_iter": [500, 1000, 1500, 2000, 5000, 8000, 10000]
+        }
 
-sgd_model = SGDClassifier(random_state=42)
-kneighbours_model = KNeighborsClassifier()
+linearsvc_random_tuned = RandomizedSearchCV(
+        linearsvc_model,
+        linearsvc_tuning_parameters,
+        n_iter = 500,
+        random_state = 42,
+        cv=5,
+        scoring="f1_micro",
+        verbose=True,
+        n_jobs=-1,
+        iid=True,
+        error_score=0.0 # needed to not get error from bad combinations
+        )
 
-long_sgd_tuning_parameters = {
-    'alpha':[0.1, 0.01, 0.001, 0.0001, 0.00001], 
-    'penalty': ('l2', 'l1', 'elasticnet'),
-    'max_iter':[10, 20 ,30, 50, 70, 100, 500, 1000],
-    'l1_ratio': [0.10, 0.15, 0.20, 0.30, 0.5, 0.85],
-    'learning_rate':('constant', 'optimal', 'invscaling', 'adaptive'),
-    'eta0': [0.1, 0.2, 0.3]
-}
+linearsvc_grid_tuned = GridSearchCV(
+        linearsvc_model,
+        linearsvc_tuning_parameters,
+        cv=5,
+        scoring="f1_micro",
+        verbose=True,
+        n_jobs=-1,
+        iid=True,
+        error_score=0.0)
 
-kn_tuning_parameters = {
-    'algorithm': ('auto', 'ball_tree', 'kd_tree'),
-    'leaf_size': [10, 30, 50],
-    'n_neighbors': [2, 3, 5],
-    'weights': ('uniform', 'distance')
-    
-}
-
-sgd_tuning_parameters = {
-    'alpha':[0.01, 0.001, 0.0001], 
-    'penalty': ('l2', 'l1', 'elasticnet'),
-    'max_iter':[10, 100, 1000],
-    'l1_ratio': [0.15,0.5,0.85]
-}
-
-sgd_CV=5
-VERBOSE=0
 
 start = time()
-sgd_random_tuned = RandomizedSearchCV(
-    sgd_model, 
-    sgd_tuning_parameters, 
-    n_iter=1, 
-    random_state=42, 
-    cv=sgd_CV, 
-    scoring='f1_micro', 
-    verbose=VERBOSE, 
-    n_jobs=-1, 
-    iid=True
-)
-#sgd_random_tuned.fit(X_train, y_train)
-#sgd_t = time()-start
 
-kn_random_tuned = RandomizedSearchCV(
-    kneighbours_model, 
-    kn_tuning_parameters, 
-    n_iter=1, 
-    random_state=42, 
-    cv=sgd_CV, 
-    scoring='f1_micro', 
-    verbose=VERBOSE, 
-    n_jobs=-1, 
-    iid=True
-)
-kn_random_tuned.fit(X_train, y_train)
-kn_t = time()-start
+#linearsvc_grid_tuned.fit(X_imgs_training, y_training)
 
-# Report result
-# sgd_b0, sgd_m0 = FullReport(sgd_random_tuned , X_test, y_test, t)
+# linearsvc_random_tuned.fit(X_imgs_training, y_training)
 
-kn_b0, kn_m0 = FullReport(kn_random_tuned , X_test, y_test, t)
-print(kn_b0)
-#print(sgd_b0)
+linearsvc_t = time()-start
+
+#linearsvc_b0, linearsvc_m0 = FullReport(linearsvc_random_tuned, X_imgs_test, y_test, linearsvc_t)
+
+#linearsvc_b0, linearsvc_m0 = FullReport(linearsvc_grid_tuned, X_imgs_test, y_test, linearsvc_t)
+
